0cfb0cc7fd40d5fe6eed190a83fa74df6bd49d56
Maksim Masterenko <mmasterenko@gmail.com>
2015-09-10 13:26:08 +0300

    admin: progress

--- vapp/admin.py
+++ vapp/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
 from models import Category, Assortiment
 
-admin.site.register(Category)
-admin.site.register(Assortiment)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'order')
+
+
+class AssortimentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'weight', 'weight_units', 'pcs', 'days', 'is_hit', 'is_new', 'is_comingSoon',
+                    'file', 'img')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Assortiment, AssortimentAdmin)
--- vapp/models.py
+++ vapp/models.py
@@ -4,18 +4,32 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    def __unicode__(self):
+        return '%s' % self.name
+    help_text = u'Позволяет менять порядок следования в меню. По умолчанию 0 ' \
+                u'(в этом случае используется стандартный порядок)'
+    name = models.CharField(u'Название', max_length=100)
+    order = models.PositiveSmallIntegerField(u'Порядок', default=0, help_text=help_text)
 
 
 class Assortiment(models.Model):
-    UNITS = ((u'г', u'г'), (u'кг', u'кг'))
+    def __unicode__(self):
+        return '%s' % self.name
 
-    category = models.ForeignKey(Category)
-    name = models.CharField(max_length=200)
-    weight = models.PositiveSmallIntegerField()
-    weightUnits = models.CharField(max_length=3 ,choices=UNITS)
-    pcs = models.DecimalField(max_digits=3, decimal_places=1)
-    days = models.PositiveSmallIntegerField()
-    isHit = models.BooleanField()
-    isNew = models.BooleanField()
-    isComingSoon = models.BooleanField()
+    WEIGHT_UNITS = ((u'г', u'г'), (u'кг', u'кг'))
+
+    category = models.ForeignKey(Category, verbose_name=u'Категория')
+    name = models.CharField(u'Название', max_length=200)
+
+    weight = models.DecimalField(u'Вес единицы', max_digits=3, decimal_places=1)
+    weight_units = models.CharField(u'Ед изм', max_length=3, choices=WEIGHT_UNITS, default=u'кг')
+
+    pcs = models.DecimalField(u'Единиц в ящике', max_digits=3, decimal_places=1, null=True, blank=True)
+    days = models.PositiveSmallIntegerField(u'Дней', default=45)
+
+    is_hit = models.BooleanField(u'Хит')
+    is_new = models.BooleanField(u'Новинка')
+    is_comingSoon = models.BooleanField(u'Скоро в продаже')
+
+    file = models.FileField(u'Файл', default='', upload_to='original')
+    img = models.ImageField(u'Картинка', upload_to='img', default='')
