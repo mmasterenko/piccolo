96863db203666fe8953db7f6b157e59f070e2908
Maksim Masterenko <mmasterenko@gmail.com>
2015-09-16 20:51:59 +0300

    assortiment page: flags is_hit, is_new, etc

--- vapp/helpers.py
+++ vapp/helpers.py
@@ -10,7 +10,11 @@ def assortiment_queryset_to_structure(assortiment_queryset):
             'pcs_weight': str(assort.weight),
             'weight_units': assort.weight_units,
             'pcs_per_box': str(assort.pcs) if assort.pcs else '',
-            'shelf_life': str(assort.days)
+            'shelf_life': str(assort.days),
+            'is_hit': assort.is_hit,
+            'is_new': assort.is_new,
+            'is_comingSoon': assort.is_comingSoon,
+            'is_someFlag': assort.is_hit or assort.is_new or assort.is_comingSoon
         }
         row.append(d)
         if len(row) >= row_length:
