5795abbfd80cf3ca3b7519314660ff8874c6f83f
Maksim Masterenko <mmasterenko@gmail.com>
2015-10-06 20:39:11 +0300

    pages admin disable actions

--- pages/admin.py
+++ pages/admin.py
@@ -4,10 +4,27 @@ from pages.models import About, Contact, Job, Distributor, General
 
 class GeneralAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'phone', 'email', 'address')
+    actions = None
 
 
-admin.site.register(About)
-admin.site.register(Job)
-admin.site.register(Contact)
-admin.site.register(Distributor)
+class AboutAdmin(admin.ModelAdmin):
+    actions = None
+
+
+class JobAdmin(admin.ModelAdmin):
+    actions = None
+
+
+class ContactAdmin(admin.ModelAdmin):
+    actions = None
+
+
+class DistributorAdmin(admin.ModelAdmin):
+    actions = None
+
+
+admin.site.register(About, AboutAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Distributor, DistributorAdmin)
 admin.site.register(General, GeneralAdmin)
